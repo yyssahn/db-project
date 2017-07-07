47ae3cf3d74cb577844339c188af368908b11d02
Kyle <yyssahn@hotmail.com>
2017-07-07 12:07:50 -0700

    added placeholder for form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }  from './app.component';
+import { DuberFormComponent } from './duber-form.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [
+    BrowserModule,
+    HttpModule
+    ],
+  declarations: [ 
+    AppComponent,
+    DuberFormComponent
+   ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/duber-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+@Component({
+  selector:'db-form',
+  templateUrl: './duber-form.component.html',
+  styleUrls: ['./duber-form.component.css']
+})
+
+
+export class DuberFormComponent {
+
+
+
+}
